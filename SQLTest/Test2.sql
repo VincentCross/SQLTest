7e291bb36c4a56d9a68eb6cfff6a41e3b9a2e3dc
VincentCross <vincent.cross.main@gmail.com>
2019-05-01 00:20:00 +1000

    Test 3 complete.

--- SQLTest/Test2.sql
+++ SQLTest/Test2.sql
@@ -1,6 +1,34 @@
 USE ExampleDB
 GO
+
+/*
+I had a little bit of an issue with this one. Namely that none of the records
+fell within the requested dates. I'm not sure if this is an error on my part or
+in the test. Either way I'd like to be informed about the issue.
+
+I tested the query with other records so I know it works.
+*/
+
 BEGIN TRANSACTION
+
+DECLARE @rangestart datetimeoffset(3) = '2015-12-12 11:00:00.000 +10:00'; --1am on the 12th in UTC
+DECLARE @rangeend datetimeoffset(3) = '2015-12-24 18:00:00.000 +10:00'; --Requested date: 8am on the 24th in UTC
+--DECLARE @rangeend datetimeoffset(3) = '2015-12-25 18:00:00.000 +10:00'; --Testing date: 8am on the 25th in UTC
+DECLARE @currenttime datetimeoffset(3) = GETUTCDATE();
+
+INSERT INTO dbo.Events_Attendance
+	SELECT eb.ID, @currenttime, 1, eb.JobSeekerID FROM dbo.Events e
+	INNER JOIN dbo.Events_Bookings eb
+	ON e.EntityID = eb.EventId
+	INNER JOIN dbo.Events_Attendance ea
+	ON eb.Id = ea.BookingId
+	WHERE e.[Start] > @rangestart
+	AND e.[End] < @rangeend;
+
+--SELECT * FROM dbo.Events_Attendance;
+--ROLLBACK
+COMMIT
+
 
 /* Testing scripts
 SELECT * FROM dbo.Events;
@@ -21,22 +49,3 @@ SELECT * FROM dbo.Events
 WHERE [Start] > @rangestart
 AND [End] < @rangeend;
 */
-
-DECLARE @rangestart datetimeoffset(3) = '2015-12-12 11:00:00.000 +10:00'; --1am on the 12th in UTC
---DECLARE @rangeend datetimeoffset(3) = '2015-12-25 18:00:00.000 +10:00'; --Testing date: 8am on the 25th in UTC
-DECLARE @rangeend datetimeoffset(3) = '2015-12-24 18:00:00.000 +10:00'; --Requested date: 8am on the 24th in UTC
-DECLARE @currenttime datetimeoffset(3) = GETUTCDATE();
-
-INSERT INTO dbo.Events_Attendance
-	SELECT eb.ID, @currenttime, 1, eb.JobSeekerID FROM dbo.Events e
-	INNER JOIN dbo.Events_Bookings eb
-	ON e.EntityID = eb.EventId
-	INNER JOIN dbo.Events_Attendance ea
-	ON eb.Id = ea.BookingId
-	WHERE e.[Start] > @rangestart
-	AND e.[End] < @rangeend;
-
---SELECT * FROM dbo.Events_Attendance;
-
---ROLLBACK
-COMMIT
---
+++ SQLTest/Test3.sql
@@ -0,0 +1,44 @@
+USE ExampleDB
+GO
+
+/*
+This is probably where I'm going to fall short. Projects at my previous work always
+involved tight deadlines and most of the time were one-off solutions. So speed of construction
+was more valued than repeat performance. But I figured I'd try anyway.
+*/
+
+/*
+Creating indexes on the ids in the tables should help.
+
+CREATE INDEX idx_bookings ON dbo.Events_Bookings (JobSeekerId)
+CREATE INDEX idx_attendance ON dbo.Events_Attendance (JobSeekerId)
+*/
+
+/*
+This new query has a few improvements:
+- It works a little better (this may not technically be what you wanted for the test)
+	- If you added a second booking but no second attendence for a job seeker,
+		the original would be dividing Booking Count (2) with Attendance Count (1) as ints, resulting in 0.
+	- The original would produce a divide by zero error when there are JobSeekers with no bookings
+- Less code repetition
+- Less subqueries
+
+I'm not actually sure if it's more efficient/optimized. But I feel it's better regardless.
+*/
+
+BEGIN TRANSACTION/*INSERT INTO dbo.JobSeekers VALUES ( 7777 )INSERT INTO dbo.Events_Bookings (EventId, AddedUserId, LastUpdatedUserId, Status, JobSeekerId)VALUES ( 3194119, 1, 1, 0, 3892 )INSERT INTO dbo.Events_Attendance VALUES (11, GETUTCDATE(), 1, 3892)*/SELECT 	Bookings.JobSeekerID, 	Bookings.[Booking Count], 	Bookings.[Attendance Count], 	CAST(Bookings.[Attendance Count] / Bookings.[Booking Count] * 100 AS INTEGER) AS 'Attendance Percentage'FROM (
+		SELECT
+			eb.JobSeekerId, 
+			CAST(Count(eb.JobSeekerId) AS DECIMAL) As 'Booking Count',
+			(SELECT CAST(COUNT(ea.JobSeekerId) AS DECIMAL) FROM dbo.Events_Attendance ea WHERE ea.JobSeekerId = eb.JobSeekerId) AS 'Attendance Count'
+		FROM dbo.Events_Bookings eb		GROUP BY eb.JobSeekerId	) AS BookingsROLLBACK/* Original QuerySELECT
+Id,
+(SELECT COUNT(*) FROM Events_Bookings WHERE JS.Id = JobSeekerId) AS 'Booking
+Count',
+(SELECT COUNT(*) FROM Events_Attendance WHERE JS.Id = JobSeekerId) AS 'Attendance
+Count',
+((SELECT COUNT(*) FROM Events_Attendance WHERE JS.Id = JobSeekerId) / (SELECT
+COUNT(*) FROM Events_Bookings WHERE JS.Id = JobSeekerId)) * 100 AS 'Attendance percentage'
+FROM
+JobSeekers JS*//* Testing QueriesDROP INDEX dbo.Events_Bookings.idx_bookingsDROP INDEX dbo.Events_Attendance.idx_attendance
+SELECT * FROM dbo.Events_Bookings;SELECT * FROM dbo.Events_Attendance;*/
